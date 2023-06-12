8a0ab26e6d14d20c3968c0c1b912fef4a8771dc1
Pravin <pravinkasabe.pk145@gmail.com>
2023-06-12 23:43:03 +0530

    second changes

--- src/com/velocity/Hello.java
+++ src/com/velocity/Hello.java
@@ -3,5 +3,8 @@ package com.velocity;
 public class Hello {
    public static void main(String[] args) {
 	System.out.println("Hello How are you.....");
+	
+	//2nd changes
+	System.out.println("Hello @nd changes.....");
 }
 }
