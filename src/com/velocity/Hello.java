b3e5ca74c9dcb5d38436f9b9e597d131fbba00af
pravink123 <121342827+pravink123@users.noreply.github.com>
2023-06-18 23:36:36 +0530

    Update Hello.java

--- src/com/velocity/Hello.java
+++ src/com/velocity/Hello.java
@@ -3,7 +3,8 @@ package com.velocity;
 public class Hello {
    public static void main(String[] args) {
 	System.out.println("Hello How are you.....");
-	
+	//dev 2
+	   System.out.println("Changes done by other developer.....");
 	//2nd changes
 	System.out.println("Hello @nd changes.....");
 	
